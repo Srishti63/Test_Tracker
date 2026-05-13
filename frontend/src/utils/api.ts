9bcc2e4fbdd2b4e06d1451a56838c83644c94a01
Srishti <srishtigupta28aug@gmail.com>
2026-05-13 22:23:42 +0530

    chore: update API base URL to use environment variable and add spacing to interceptor

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL : 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_API_URL,
 })
 
 api.interceptors.request.use((config) =>{
@@ -9,6 +9,7 @@ api.interceptors.request.use((config) =>{
     if(token){
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 });
 
